0dbc5afc69ad5375d1f0c8092a37efeeecf5f064
ismail-zdn <57730886+ismail-zdn@users.noreply.github.com>
2020-09-18 00:16:42 +0300

    Add files via upload

---
+++ assignment_2_Variables/Question6.java
@@ -0,0 +1,22 @@
+package assignment_2_Variables;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String person1,person2,person3,person4,person5;
+		
+		person1="James";
+		person2="Betty";
+		person3="Bob";
+		person4="Herb";
+		person5="Jill";
+		
+		// bob+jill+betty+herb+james
+		
+		System.out.println(person3+" , "+person5+" , "+person2+" , "+person4+" , "+person1);
+
+	}
+
+}
