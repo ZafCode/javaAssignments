eaed7d0db9a23fe3c119ed9a53fe28a10230f7bd
Ismail Ozden <57730886+ismail-zdn@users.noreply.github.com>
2020-11-18 20:47:05 +0100

    Add files via upload

---
+++ assignment_8/Question07.java
@@ -0,0 +1,21 @@
+package assignments.assignment_8;
+
+public class Question07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+	
+	public static void isPalindrome(int number) {
+		
+		
+			
+			
+		}
+		
+	}
+
+
